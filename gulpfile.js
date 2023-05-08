1e59f59f882c47ab634c76d109d0e1f7cc7fd416
i1last <ac7you@gmail.com>
2023-05-08 18:44:52 +0300

    fix src path

--- gulpfile.js
+++ gulpfile.js
@@ -89,7 +89,7 @@ function jsCompile() {
 }
 
 function scssCompile() {
-  return src('app/assets/scss/components/desktop/_header.scss')
+  return src('app/assets/scss/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sassBulkImporter())
     .pipe(concat('main.min.scss'))
