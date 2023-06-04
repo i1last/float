fa2c587b27143029ebda18e66301c9847b981237
i1last <ac7you@gmail.com>
2023-06-04 12:18:14 +0300

    replace hamburger-menu to navigate-menu

---
+++ app/assets/js/navigate-menu.js
@@ -0,0 +1,20 @@
+const hamburgerButton = document.querySelector('.header__hamburger-button');
+let navigateMenu = document.querySelector('.navigate-menu');
+let container = document.querySelector('.container');
+
+hamburgerButton.addEventListener('click', () => {
+  if (navigateMenu.classList.contains('navigate-menu--showing')) {
+    navigateMenu.classList.remove('navigate-menu--showing');
+    container.style.display = 'inherit';
+  } else {
+    navigateMenu.classList.add('navigate-menu--showing');
+    container.style.display = 'none';
+  }
+})
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 600) {
+        navigateMenu.classList.remove('navigate-menu--showing');
+        container.style.display = 'inherit';
+    }
+})
