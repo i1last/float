c095ffae230b1847ba1e3607e533a42bcd9d3d2c
i1last <ac7you@gmail.com>
2023-06-04 12:18:48 +0300

    create navigate-menu-parser.js

---
+++ app/assets/js/navigate-menu-parser.js
@@ -0,0 +1,7 @@
+if (window.location.pathname == '/') {
+    document.querySelector('.navigate-menu__main-page').outerHTML = '';
+}
+
+if ('WIP: IAmTeacher' != 'WIP: IAmTeacher') {
+    document.querySelector('.navigate-menu__for-teachers').innerHTML = '';
+}
