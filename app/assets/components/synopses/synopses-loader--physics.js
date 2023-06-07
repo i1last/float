184152ad8781b62c8c3ee24b6374f767ce6c351d
i1last <ac7you@gmail.com>
2023-06-07 23:48:59 +0300

    fix path in synopses-loader--physics.js

--- app/assets/components/synopses/synopses-loader--physics.js
+++ app/assets/components/synopses/synopses-loader--physics.js
@@ -4,7 +4,7 @@ let ul = document.createElement('ul');
 ul.classList.add('synopses__list', 'slides__list');
 
 
-fetch('/database/handbook/physics/synopses/cards.json').then((res) => res.json()).then((cards) => {
+fetch('/database/pages/handbook/physics/synopses/cards.json').then((res) => res.json()).then((cards) => {
     for (const item of cards) {
         let li = document.createElement('li');
         li.classList.add('synopses__item', 'slide', 'slide--medium', ((item.grade == 10) ? 'bg--blueberry' : 'bg--brown'));
@@ -16,8 +16,8 @@ fetch('/database/handbook/physics/synopses/cards.json').then((res) => res.json()
             <div class="slide__image slide__download download-aside">
             <h3 class="download-aside__title">Скачать</h3>
             <ul class="download-aside__list">
-                <li><object><a class="download-aside__download-item bg--green" href="/database/handbook/physics/synopses/${item.grade}/${item.grade}-${item.card}.pdf" download>pdf</a></object></li>
-                <li><object><a class="download-aside__download-item bg--brown" href="/database/handbook/physics/synopses/${item.grade}/${item.grade}-${item.card}.svg" download>svg</a></object></li>
+                <li><object><a class="download-aside__download-item bg--green" href="/database/pages/handbook/physics/synopses/${item.grade}/${item.grade}-${item.card}.pdf" download>pdf</a></object></li>
+                <li><object><a class="download-aside__download-item bg--brown" href="/database/pages/handbook/physics/synopses/${item.grade}/${item.grade}-${item.card}.svg" download>svg</a></object></li>
             </ul>
             </div>
         </a>
