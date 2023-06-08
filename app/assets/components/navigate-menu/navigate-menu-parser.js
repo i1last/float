8133c52f213f6ceb6079ec109cc2ca9d457ff7f5
i1last <ac7you@gmail.com>
2023-06-08 13:06:46 +0300

    add new system: scrollable

--- app/assets/components/navigate-menu/navigate-menu-parser.js
+++ app/assets/components/navigate-menu/navigate-menu-parser.js
@@ -1,7 +1,3 @@
 if (window.location.pathname == '/') {
     document.querySelector('.navigate-menu__main-page').outerHTML = '';
 }
-
-if ('WIP: IAmTeacher' != 'WIP: IAmTeacher') {
-    document.querySelector('.navigate-menu__for-teachers').innerHTML = '';
-}
