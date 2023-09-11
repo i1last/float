0e900a0fe7d9ac204ab1726e195ac701be584823
i1last <ac7you@gmail.com>
2023-09-11 19:49:27 +0300

    feat: update synopses loader js

--- app/assets/components/synopses/synopses-loader--physics.js
+++ app/assets/components/synopses/synopses-loader--physics.js
@@ -17,15 +17,15 @@ for (const item of cards) {
     li.classList.add('synopses__item', 'slide', 'slide--medium', ((item.grade == 10) ? 'bg--blueberry' : 'bg--brown'));
 
     li.innerHTML = `
-    <a class="slide__link slide__link--p-large" href="./?image=${item.grade}/${item.grade}-${item.card}.svg">
+    <a class="slide__link slide__link--p-large" href="./?image=${item.grade}/card_${item.grade}-${item.card}.svg">
         <div class="slide__title slide__title--fz-normal slide__title--mb-normal">Карточка ${item.grade}/${item.card}</div>
         <div class="slide__description slide__description--fz-normal slide__description--maw-medium">${item.description}</div>
         <div class="slide__image slide__download download-aside" id="js-download-aside--${i}">
             <object><a class="download-aside__open" js-download-aside-id="${i}" href="#!">Скачать</a></object>
             <h3 class="download-aside__title">Скачать</h3>
             <ul class="download-aside__list">
-                <li><object><a class="download-aside__download-item bg--green" href="/database/pages/handbook/physics/synopses/${item.grade}/${item.grade}-${item.card}.pdf" download>pdf</a></object></li>
-                <li><object><a class="download-aside__download-item bg--brown" href="/database/pages/handbook/physics/synopses/${item.grade}/${item.grade}-${item.card}.svg" download>svg</a></object></li>
+                <li><object><a class="download-aside__download-item bg--green" href="/database/pages/handbook/physics/synopses/${item.grade}/card_${item.grade}-${item.card}.pdf" download>pdf</a></object></li>
+                <li><object><a class="download-aside__download-item bg--brown" href="/database/pages/handbook/physics/synopses/${item.grade}/card_${item.grade}-${item.card}.svg" download>svg</a></object></li>
             </ul>
         </div>
     </a>
